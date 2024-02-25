bc343bfedd3c307c2c6047b96d0ec127b615ddb0
Jamoliddin <jamoliddintirkashaliyev04@gmail.com>
2024-02-25 13:22:06 +0500

    Transfer loyhasi toliq tugatildi THE END

--- Transfers/urls.py
+++ Transfers/urls.py
@@ -6,13 +6,23 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index, name='home'),
-    path('clubs/', clubs, name='clubs'),
-    path('palyers/', players, name='players'),
-    path('lastest_transfers/', lastest_transfers, name='lastest_transfers'),
-    path('clubs/', clubs, name='clubs'),
-    path('transfer_records/', transfer_records),
-    path('<str:country_name>/clubs/',country_clubs),
+                  path('admin/', admin.site.urls),
+                  path('', index, name='home'),
+                  path('clubs/', clubs, name='clubs'),
+                  path('stats/', stats, name='stats'),
+                  path('palyers/', players, name='players'),
+                  path('about/', about, name='about'),
+                  path('lastest_transfers/', lastest_transfers, name='lastest_transfers'),
+                  path('clubs/', clubs, name='clubs'),
+                  path('players20/', u_20, name='u_20'),
+                  path('tryouts/', tryouts, name='tryouts'),
+                  path('archive/', transfer_archive, name='archive'),
+                  path('season/', season2017_18, name='season2017_18'),
+                  path('season/', season2018_19, name='season2018_19'),
+                  path('season/', season2019_20, name='season2019_20'),
+                  path('season/', season2020_21, name='season2020_21'),
+                  path('transfer_records/', transfer_records),
+                  path('<str:country_name>/clubs/', country_clubs),
+                  path('<str:nom>/players/', clubs_players),
 
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- mainApp/views.py
+++ mainApp/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from mainApp.models import *
+from datetime import date
 
 
 def index(request):
@@ -26,6 +27,8 @@ def country_clubs(request, country_name):
         'clubs': Club.objects.filter(davlat__nom__contains=country_name)
     }
     return render(request, 'country-clubs.html', context)
+
+
 def lastest_transfers(request):
     context = {
         'transfers': Transfer.objects.order_by('-sana')
@@ -33,11 +36,13 @@ def lastest_transfers(request):
     }
     return render(request, 'latest-transfers.html', context)
 
+
 def transfer_records(request):
     context = {
         'transfers': Transfer.objects.filter(narx__gt=50)
     }
     return render(request, 'transfer-records.html', context)
+
 
 def clubs(request):
     context = {
@@ -45,3 +50,68 @@ def clubs(request):
     }
     return render(request, 'clubs.html', context)
 
+
+def clubs_players(request, nom):
+    context = {
+        'players': Player.objects.filter(club=Club.objects.filter(nom=nom).first())
+    }
+    return render(request, 'clubs-players.html', context)
+
+
+def stats(request):
+    return render(request, 'stats.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def u_20(request):
+    hozirgi_sana = str(date.today())
+    yil = int(hozirgi_sana[:4]) - 20
+    yangi_sana = hozirgi_sana.replace(hozirgi_sana[:4], str(yil))
+    players = Player.objects.filter(t_yil__gt=yangi_sana)
+    context = {
+        'players': players
+    }
+    return render(request, 'U-20 players.html', context=context)
+
+
+def tryouts(request):
+    return render(request, 'tryouts.html')
+
+
+def transfer_archive(request):
+    return render(request, 'transfer-archive.html')
+
+
+def season2017_18(request):
+    context = {
+        'transfers': Transfer.objects.filter(mavsum='2017/2018')
+
+    }
+    return render(request, '2017-18season.html', context)
+
+
+def season2018_19(request):
+    context = {
+        'transfers': Transfer.objects.filter(mavsum='2018/2019')
+
+    }
+    return render(request, '2017-18season.html', context)
+
+
+def season2019_20(request):
+    context = {
+        'transfers': Transfer.objects.filter(mavsum='2019/2020')
+
+    }
+    return render(request, '2017-18season.html', context)
+
+
+def season2020_21(request):
+    context = {
+        'transfers': Transfer.objects.filter(mavsum='2020/2021')
+
+    }
+    return render(request, '2017-18season.html', context)
